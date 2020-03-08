896d4161e63b5a8835d49171280897db91cf355c
Sujit <sujit.mollet@rutgers.edu>
2020-03-07 23:24:43 -0500

    conflict

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -3,7 +3,7 @@ package chess;
 public class Chess {
 
     public static void main(String[] args) {
-	    System.out.println("This is our chess game!");
+	    System.out.println("This is our Chess game!");
 
     }
 }
