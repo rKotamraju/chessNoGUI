a33e420294c562db8e02076e0af22470ec43953e
Sujit <sujit.mollet@rutgers.edu>
2020-03-16 23:34:37 -0400

    Rook can move, can't kill

--- src/chess/Rook.java
+++ src/chess/Rook.java
@@ -11,7 +11,38 @@ public class Rook extends Piece{
     public boolean move(String omove, String nmove, Piece[][] board, int oFile, int oRank, int nFile, int nRank)  {
         System.out.println("Player is moving a Rook");
 
+        if(logicalXOR(oFile == nFile, oRank == nRank)){ //Either Files have to be the same or Ranks have to be the but not both
+
+            int minRank = Math.min(oRank, nRank);
+            int maxRank = Math.max(oRank, nRank);
+
+            int minFile = Math.min(oFile, nFile);
+            int maxFile = Math.max(oFile, nFile);
+
+            if(oRank != nRank){ //vertical move
+                for(int i = minRank+1; i< maxRank; i++){ //plus one bc we do not want to check the spot of the rook
+                    if (board[i][oFile] != null) {
+                        System.out.println("Something in the way: "+i+","+oFile);
+                        return false;
+                    }
+                }
+            } else { //horizontal move
+                for(int i = minFile+1; i< maxFile; i++){
+                    if (board[oRank][i] != null) {
+
+                        System.out.println("Something in the way");
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
 
         return false;
+    }
+
+    public static boolean logicalXOR(boolean x, boolean y) {
+        return ( ( x || y ) && ! ( x && y ) );
     }
 }
