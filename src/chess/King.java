d628643ba3bdc57f312af999e6cfac7e6b284775
Sujit <sujit.mollet@rutgers.edu>
2020-03-18 20:04:09 -0400

    done with King movement

--- src/chess/King.java
+++ src/chess/King.java
@@ -1,17 +1,23 @@
 package chess;
 
-public class King extends Piece{
+public class King extends Queen{
 
     public King(boolean isWhite){
-        this.isWhite = isWhite;
+        super(isWhite);
         this.name = isWhite ? "wK" : "bK";
     }
 
     @Override
     public boolean move(Piece[][] board, int oFile, int oRank, int nFile, int nRank)  {
-        System.out.println("Player is moving a King");
 
+        int fdist = Math.abs(oFile - nFile);
+        int rdist = Math.abs(oRank - nRank);
 
-        return false;
+        if(fdist > 1 || rdist > 1){
+            System.out.println("King can only move one space");
+            return false;
+        }
+
+        return super.move(board, oFile, oRank, nFile, nRank);
     }
 }
